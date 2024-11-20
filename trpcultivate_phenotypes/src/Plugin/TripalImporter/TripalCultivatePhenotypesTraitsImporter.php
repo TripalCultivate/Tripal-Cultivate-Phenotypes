a556752611426cc80ba0c2f1f597fb498fde2efa
Carolyn Caron <carolyn.caron@usask.ca>
2024-11-20 14:37:27 -0600

    Removed a Drupal service call in run() and just used the service directly

--- trpcultivate_phenotypes/src/Plugin/TripalImporter/TripalCultivatePhenotypesTraitsImporter.php
+++ trpcultivate_phenotypes/src/Plugin/TripalImporter/TripalCultivatePhenotypesTraitsImporter.php
@@ -735,13 +735,10 @@ class TripalCultivatePhenotypesTraitsImporter extends ChadoImporterBase implemen
    * {@inheritDoc}
    */
   public function run() {
-    // Traits service.
-    $service_traits = \Drupal::service('trpcultivate_phenotypes.traits');
-
     // Values provided by user in the importer page.
     $genus = $this->arguments['run_args']['genus'];
     // Tell trait service that all trait assets will be contained in this genus.
-    $service_traits->setTraitGenus($genus);
+    $this->service_PhenoTraits->setTraitGenus($genus);
     // Traits data file id.
     $file_id = $this->arguments['files'][0]['fid'];
     // Load file object.
@@ -782,7 +779,7 @@ class TripalCultivatePhenotypesTraitsImporter extends ChadoImporterBase implemen
         // NOTE: Loading of this file is performed using a database transaction.
         // If it fails or is terminated prematurely, then all insertions and
         // updates are rolled back and will not be found in the database.
-        $service_traits->insertTrait($trait);
+        $this->service_PhenoTraits->insertTrait($trait);
 
         unset($data);
       }
